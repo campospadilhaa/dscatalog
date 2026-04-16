7056016c8fd3860cf42b5952ab1e6012133cf7fd
campospadilha.a <campospadilha.a@gmail.com>
2026-04-16 11:46:43 -0300

    Criado service.findCategoryById. Criado também o controle de exceção

--- backend/dscatalog/src/main/java/com/campospadilhaa/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/campospadilhaa/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.campospadilhaa.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.campospadilhaa.dscatalog.dto.CategoryDTO;
 import com.campospadilhaa.dscatalog.entities.Category;
 import com.campospadilhaa.dscatalog.repositories.CategoryRepository;
+import com.campospadilhaa.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -29,5 +31,21 @@ public class CategoryService {
 					.collect(Collectors.toList());
 
 		return listCategoryDTO;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+
+		Optional<Category> optCategory = repository.findById(id);
+
+		// Category category = optCategory.get();
+
+		Category category =
+				optCategory
+					.orElseThrow( () -> new EntityNotFoundException("Categoria não encontrada") );
+
+		CategoryDTO categoryDTO = new CategoryDTO(category);
+
+		return categoryDTO;
 	}
 }
